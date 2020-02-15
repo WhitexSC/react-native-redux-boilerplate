df9c3aadd6cd99b95483905d07651a2fec820d3e
Roman Tyrtyshnyy <rtyrtyshnyy@melontech.com>
2020-02-15 16:07:24 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hihi startPage</Text>
+    </View>
+  );
+};
+
+
+export default App;
