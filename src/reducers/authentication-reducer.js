a3c213b572a39927886be6bc0f325a9cc21412ff
WhitexSC <whitex1337@gmail.com>
2020-08-11 17:42:10 +0300

    initial reducer update

---
+++ src/reducers/authentication-reducer.js
@@ -0,0 +1,12 @@
+import { DEFAULT_AUTHENTICATION_STATE } from "../config/defaultStates";
+
+
+export default function authentication(
+  state = DEFAULT_AUTHENTICATION_STATE,
+  action
+) {
+  switch (action.type) {
+    case "ADD_NEW_DEVICE":
+      return
+  }
+};
